05812ef35f56fba5b9fdfcd07d603952c18dd4f6
Jose <pedrojcunha7@hotmail.com>
2025-11-07 09:48:25 +0100

    done project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count === 0) return;
+  }, [count]);
+
   return (
-    <main>
-      <div>Hello world!</div>
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Compteur : {count}</h1>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(0)} style={{ marginLeft: "10px" }}>
+        Reset maintenant
+      </button>
     </main>
   );
 }
