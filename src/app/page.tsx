31797947bba3e66a93c678dcdc07f8b03158e088
Jose <pedrojcunha7@hotmail.com>
2025-11-07 09:51:34 +0100

    empty project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
+  // ici créez un useState qui va garder la valeur des click, et un use effect qui va être appelé uniquement quand on click sur "reset"
   return (
+    // affichez après main 2 bouton (1 pour le compteur 1 pour le reset) et un h1 qui contient la valeur des click.
     <main>
-      <div>Hello world!</div>
     </main>
   );
 }
+
+// verifie ta réponse à la fin en fesant un "git checkout done-project"
+// pour revenir ici après avoir vérifier la réponse fait un "git checkout main"
