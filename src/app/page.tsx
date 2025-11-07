00023f18dbb16f052043a6727c516269ba642c4e
Jose <pedrojcunha7@hotmail.com>
2025-11-07 10:29:57 +0100

    empty project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-  // ici créez un useState qui va garder la valeur des click, et un use effect qui va être appelé uniquement quand on click sur "reset"
+  // ici créez un useState qui va garder la valeur des click, et un use effect qui va être appelé uniquement quand on click sur "reset" si le compteur est plus haut que 0
   return (
     // affichez après main 2 bouton (1 pour le compteur, 1 pour le reset) et un h1 qui contient la valeur des click.
     <main>
