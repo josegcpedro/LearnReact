7f1917024a2fb041d0dbc1e5e0410c270b6e7c77
Jose <pedrojcunha7@hotmail.com>
2025-11-14 09:24:14 +0100

    done project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
+import Props from "./props";
 export default function Home() {
   const [count, setCount] = useState(0);
+  const name: string = "Jose";
 
   useEffect(() => {
     if (count > 0) {
       alert(`Le compteur est maintenant à ${count}`);
     }
-  }, [count]); // Se déclenche à chaque changement de count
+  }, [count]); // Se déclenche à chaque changement de count.
 
   return (
     <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <Props name={name} />
       <h1>Compteur : {count}</h1>
       <button onClick={() => setCount(count + 1)}>+1</button>
       <button onClick={() => setCount(0)} style={{ marginLeft: "10px" }}>
---
+++ src/app/props.tsx
@@ -0,0 +1,3 @@
+export default function Props({ name }: { name: string }) {
+  return <h1>Salut {name}</h1>;
+}
